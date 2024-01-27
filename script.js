afd86e18d37993e09244af0303cf483689025b9b
Federica Rossi <federicarossi@Federicas-MacBook-Air.local>
2024-01-27 14:10:48 +0400

    removed js

--- script.js
+++
@@ -1,9 +0,0 @@
-const banner = document.getElementById("themes");
-const theme = document.getElementsByClassName("theme");
-
-document
-  .getElementById("paintbrush-button")
-  .addEventListener("click", function () {
-    banner.classList.toggle("themes-display");
-    theme.classList.toggle("themes-display");
-  });
